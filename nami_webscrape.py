85e1afdfc7eb6d67f69f1bddbf1c24464578b43d
Hernan Carvente <carvente.hernan@gmail.com>
2020-12-08 20:29:34 -0500

    nami py update

--- nami_webscrape.py
+++ nami_webscrape.py
@@ -1,12 +1,24 @@
 from bs4 import BeautifulSoup
 import requests
 
-source = requests.get('https://www.nami.org/Home').text
+source = requests.get('https://www.nami.org/About-Mental-Illness').text
 
 soup = BeautifulSoup(source, 'lxml')
 
 # print(soup.prettify())
 
-for resource in soup.find_all('div', class_='dropdown-menu dropdown-menu-right dropdown-menu-lg-left show'):
-	state = resource.a.data-text
-	print(state)
+# this line is looping into all class "links-region" tag
+for topic in soup.find_all('div', class_='links-region'):
+	info = topic.ul.find_all('li')
+	# print(info)
+	for link in info:
+		if link.a == None:
+			print('No href')
+		else:
+			title = link.a.get('href')
+			print(title)
+	
+
+	
+
+ 
